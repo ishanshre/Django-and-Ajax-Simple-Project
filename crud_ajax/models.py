256c0020cc222b4b31870c9c2ab5c59a1bf71abd
Ishan Shrestha <ishan.shrestha356@gmail.com>
2022-10-23 06:57:18 +0545

    Feat: create photo models     - create a photo model in foreign relationship with post model     - register photo model in admin page     - add meta class in post model for reverse ordering according to latest post     - create a method in post model to return all the photoes associated to it

--- crud_ajax/models.py
+++ crud_ajax/models.py
@@ -28,4 +28,19 @@ class Post(models.Model):
     def like_count(self):
         return self.liked.all().count()
     
+    def get_photos(self):
+        return self.post_photos.all()
+
+    class Meta:
+        ordering = ['-created'] 
+
+class Photo(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post_photos')
+    image = models.ImageField(upload_to='post/photo')
+    caption = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
     
+    def __str__(self):
+        return f"{self.post.title}-{self.pk}"
+
